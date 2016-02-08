8d9abfb862488acc4693d46d95c07391f24216fa
jiushiwosty <jiushiwosty@berkeley.edu>
2016-02-08 23:32:59 +0000

    3/4

--- app.rb
+++ app.rb
@@ -40,7 +40,7 @@ class HangpersonApp < Sinatra::Base
   post '/guess' do
     letter = params[:guess].to_s[0]
     ### YOUR CODE HERE ###
-    HangpersonGame.guess(letter)
+    @game.guess(letter)
     redirect '/show'
   end
   
@@ -51,14 +51,14 @@ class HangpersonApp < Sinatra::Base
   # wrong_guesses and word_with_guesses from @game.
   get '/show' do
     ### YOUR CODE HERE ###
-    # if HangpersonGame.check_win_or_lose == :win
-    #   redirect '/win'
-    # elsif HangpersonGame.check_win_or_lose == :lose
-    #   redirect '/lose'
-    # else
+    if @game.check_win_or_lose == :win
+      redirect '/win'
+    elsif @game.check_win_or_lose == :lose
+      redirect '/lose'
+    else
 
-    erb :show # You may change/remove this line
-    # end
+      erb :show # You may change/remove this line
+    end
   end
   
   get '/win' do
